74ab60c7379b88db581cbd0b95a474bf7f7356be
Jhon Marco Achata Limachi <jmal_444@hotmail.com>
2018-02-01 14:24:13 -0500

    simple chatbot completed

--- index.js
+++ index.js
@@ -28,9 +28,82 @@ app.post('/webhook', function(req, res) {
 	if(data.object == 'page'){
 		data.entry.forEach(function(pageEntry){
 			pageEntry.messaging.forEach(function(messagingEvent){
-
+				if (messagingEvent.message) {
+					reciveMessage(messagingEvent);
+				}
 			});
 		});
 		res.sendStatus(200);
 	}
 });
+
+function reciveMessage(event) {
+	// console.log(event);
+	const senderId = event.sender.id;
+	const messageText = event.message.text;
+
+	eveluateMessage(senderId, messageText);
+}
+
+function eveluateMessage(recipientId, message) {
+	let finalMessage = '';
+
+	if(isContain(message, 'ayuda')) {
+		finalMessage = 'Por el momento no te puedo ayudar';
+	} else if (isContain(message, 'gato')) {
+		sendMessageImage(recipientId);
+	} else if (isContain(message, 'nombre')) {
+		finalMessage = 'Yo soy OneBot, un bot de prueba :D';
+	} else {
+		finalMessage = 'Solo se repetir las cosas : ' + message;
+	}
+	sendMessageText(recipientId, finalMessage)
+}
+
+function sendMessageText(recipientId, message) {
+	const messageData = {
+		recipient: {
+			id: recipientId
+		},
+		message: {
+			text: message
+		}
+	};
+	callSendAPI(messageData);
+}
+
+function sendMessageImage(recipientId) {
+	const messageData = {
+		recipient: {
+			id: recipientId
+		},
+		message: {
+			attachment: {
+				type: "image",
+				payload: {
+					url: "https://i.imgur.com/RtC6c01.jpg"
+				}
+			}
+		}
+	};
+	callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: { access_token : config.get('pageAccessToken') },
+		method: 'POST',
+		json: messageData
+	}, function(error, response, data) {
+		if (error) {
+			console.log('No es posible enviar el mensaje');
+		} else {
+			console.log('El mensaje fue enviado');
+		}
+	});
+}
+
+function isContain(sentence, word) {
+	return sentence.indexOf(word) > -1;
+}
