fa426fddedc4b9e99f6bd2f0a8f22c1cd5e946b7
Jhon Marco Achata Limachi <jmal_444@hotmail.com>
2018-02-01 12:50:14 -0500

    webhook setup and add config package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const config = require('config');
 
 var app = express();
 
@@ -12,4 +13,24 @@ app.listen(3000, function() {
 
 app.get('/', function(req, res) {
 	res.send("Hola bienvenido a la página");
+});
+
+app.get('/webhook', function(req, res) {
+	if (req.query['hub.verify_token'] === config.get(validationToken)) {
+		res.send(req.query['hub.challenge']);
+	} else {
+		res.send("Tu no tienes que entrar aquí");
+	}
+});
+
+app.post('/webhook', function(req, res) {
+	var data = req.body;
+	if(data.object == 'page'){
+		data.entry.forEach(function(pageEntry){
+			pageEntry.messaging.forEach(function(messagingEvent){
+
+			});
+		});
+		res.sendStatus(200);
+	}
 });
